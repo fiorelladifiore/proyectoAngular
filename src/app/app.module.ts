9652ad96c9c001712061a7068f69e9844a96c320
fiorella <fiorella2difiore@gmail.com>
2025-02-20 17:51:08 -0300

    agrego componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PlantsListComponent } from './plants-list/plants-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PlantsListComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/plants-list/plants-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-plants-list',
+  standalone: false,
+  
+  templateUrl: './plants-list.component.html',
+  styleUrl: './plants-list.component.scss'
+})
+export class PlantsListComponent {
+
+}
