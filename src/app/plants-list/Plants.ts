6ef9b58ecbdf676f3601c30dbea7fee4131a7c1e
fiorella <fiorella2difiore@gmail.com>
2025-02-24 18:14:01 -0300

    agrego interfaz y directiva

---
+++ src/app/plants-list/Plants.ts
@@ -0,0 +1,7 @@
+export interface Plants{
+  image: string;
+  name: string;
+  type: string;
+  price: number;
+  stock: number;
+}
--- src/app/plants-list/plants-list.component.ts
+++ src/app/plants-list/plants-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Plants} from './Plants';
 
 @Component({
   selector: 'app-plants-list',
@@ -8,25 +9,27 @@ import { Component } from '@angular/core';
   styleUrl: './plants-list.component.scss'
 })
 export class PlantsListComponent {
-  plant = {
+  plants: Plants[] = [
+    {
     "image": "/img/monstera.png",
     "name": "Monstera deliciosa",
     "type": "Araceae",
     "price": 200,
     "stock": 3
-  }
-  plant2 = {
+  },
+  {
     "image": "/img/potus.png",
     "name": "Potus",
     "type": "Araceae",
     "price": 300,
     "stock": 4
-  }
-  plant3 = {
+  },
+  {
     "image": "/img/sansevieria.png",
     "name": "Sansevieria enana",
     "type": "Asparagaceae",
     "price": 120,
     "stock": 7
   }
+  ]
 }
