e7f4f32155607b37afdb0f947bef76220c28bd01
fiorella <fiorella2difiore@gmail.com>
2025-03-05 18:37:31 -0300

    añado home

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,4 +8,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'VIVERO';
+  email= 'difiore8fiore@gmail.com';
+  tel= '+54 226238148';
 }
--- src/app/vivero-home/vivero-home.component.ts
+++ src/app/vivero-home/vivero-home.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
 
+
+
 @Component({
   selector: 'app-vivero-home',
   standalone: false,
-  
+
   templateUrl: './vivero-home.component.html',
   styleUrl: './vivero-home.component.scss'
 })
 export class ViveroHomeComponent {
-
+  imagenes=
+    {
+      image1: "/img/1carousel.png",
+      image2: "/img/2carousel.png",
+      image3: "/img/3carousel.png",
+      image4: "/img/mediodepago.png",
+      image5: "/img/home2.png",
+      image6: "/img/home3.png",
+    }
 }
