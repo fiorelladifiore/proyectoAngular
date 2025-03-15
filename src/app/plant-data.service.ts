dbf1220393b97f7649009137e1c7f07d169ceafd
fiorella <fiorella2difiore@gmail.com>
2025-03-14 22:23:12 -0300

    modifico data service

--- src/app/plant-data.service.ts
+++ src/app/plant-data.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
-import {Observable} from 'rxjs';
+import {Observable, tap} from 'rxjs';
 import {Plants} from './plants-list/Plants';
 
 const URL= 'https://6557e65ebd4bcef8b6133c56.mockapi.io/api/plants';
@@ -13,6 +13,9 @@ export class PlantDataService {
   constructor(private http: HttpClient) { }
 
   public getAll(): Observable<Plants[]>{
-    return this.http.get<Plants[]>(URL);
+    return this.http.get<Plants[]>(URL)
+              .pipe(
+                tap((plants: Plants[]) => plants.forEach(plant => plant.quantity = 0))
+              );
   }
 }
